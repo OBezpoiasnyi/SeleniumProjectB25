60e82bf56b4398eb02e28611271c86613a7f32e3
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-02-08 14:40:42 -0600

    day 3 CSS selectors XPATH

---
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -0,0 +1,45 @@
+package com.cydeo.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class WebDriverFactory {
+
+
+        public static WebDriver getDriver(String browserType){
+
+            if(browserType.equalsIgnoreCase("chrome")){
+                WebDriverManager.chromedriver().setup();
+                WebDriver driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                return driver;
+            }else if(browserType.equalsIgnoreCase("firefox")){
+                WebDriverManager.firefoxdriver().setup();
+                WebDriver driver = new FirefoxDriver();
+                driver.manage().window().maximize();
+                return driver;
+            }else if(browserType.equalsIgnoreCase("safari")){
+                WebDriverManager.safaridriver().setup();
+                WebDriver driver = new SafariDriver();
+                driver.manage().window().maximize();
+                return driver;
+            }
+            return null;
+        }
+
+
+        // TASK: NEW METHOD CREATION
+        // Method name : getDriver
+        // Static method
+        // Accepts String arg: browserType
+        //   - This arg will determine what type of browser is opened
+        //   - if "chrome" passed --> it will open chrome browser
+        //   - if "firefox" passed --> it will open firefox browser
+        // RETURN TYPE: "WebDriver"
+
+
+
+}
